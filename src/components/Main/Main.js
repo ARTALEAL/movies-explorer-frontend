98480f1a0814f6bb6140b8e5e67a7f2451e8a783
ARTALEAL <artaleal@yandex.ru>
2023-04-14 23:49:13 +0300

    about technology desktop

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,12 @@
 import Promo from '../Promo/Promo';
+import About from '../About/About';
+import Technologies from '../Technologies/Technologies';
 const Main = ({ loggedIn }) => {
   return (
     <>
       <Promo loggedIn={loggedIn} />
+      <About />
+      <Technologies />
     </>
   );
 };
