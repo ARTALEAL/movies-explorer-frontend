360b6f30531b5fadc4cb0fedbebcac0d9ec48f6d
ARTALEAL <artaleal@yandex.ru>
2023-04-17 13:58:46 +0300

    portfolio

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -10,14 +10,14 @@ const Student = () => {
         <div className="student__info">
           <span className="student__name">Артём</span>
           <span className="student__job">Фронтенд-разработчик, 33 года</span>
-          <span className="student__about">
+          <p className="student__about">
             Я родился и живу в Санкт-Петербурге, закончил факультет экономики
             СпбГУАП по специальности "прикладная информатика в экономике". У
             меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь
             сноубордом. Недавно начал кодить. С 2012 года из ИТ-сферы перешёл в
             автобизнес, но после того, как прошёл курс по веб-разработке, решил
             вернуться в ИТ-сферу.
-          </span>
+          </p>
           <a
             className="student__link"
             href="https://github.com/ARTALEAL"
