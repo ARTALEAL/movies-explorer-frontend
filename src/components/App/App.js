afadfa76a390c25afc211f5b1f931cf4777e6007
ARTALEAL <artaleal@yandex.ru>
2023-04-20 00:22:23 +0300

    readme

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,7 +11,7 @@ import Login from '../Login/Login';
 import NotFoundPage from '../NotFoundPage/NotFoundPage';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   return (
     <div className="App">
       <Routes>
