5ceb2502c1948c4234e0bf17ac735397a77ee41c
ARTALEAL <artaleal@yandex.ru>
2023-04-22 00:55:00 +0300

    api methods

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -34,3 +34,37 @@ export const authorize = async ({ email, password }) => {
     }),
   }).then((res) => checkResponse(res));
 };
+
+export const getContent = async (jwt) => {
+  return fetch(`${BASE_API_URL}/users/me`, {
+    method: 'GET',
+    headers: {
+      ...headers,
+      Authorization: `Bearer ${jwt}`,
+    },
+  }).then((res) => checkResponse(res));
+};
+
+export const updateUserInfo = async (data, jwt) => {
+  return fetch(`${BASE_API_URL}/users/me`, {
+    method: 'PATCH',
+    headers: {
+      ...headers,
+      Authorization: `Bearer ${jwt}`,
+    },
+    body: JSON.stringify({
+      name: data.name,
+      email: data.email,
+    }),
+  }).then((res) => checkResponse(res));
+};
+
+export const getSavedMovies = async (jwt) => {
+  return fetch(`${BASE_API_URL}/movies`, {
+    method: 'GET',
+    headers: {
+      ...headers,
+      Authorization: `Bearer ${jwt}`,
+    },
+  }).then((res) => checkResponse(res));
+};
