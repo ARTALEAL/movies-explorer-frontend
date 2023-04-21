d199a359c6e221ac70b8569cca7b8903cb8212d3
ARTALEAL <artaleal@yandex.ru>
2023-04-21 23:43:48 +0300

    filter

---
+++ src/utils/utils.js
@@ -0,0 +1,15 @@
+export const convertMinToHours = (num) => {
+  const minutes = num % 60;
+  const hours = (num - minutes) / 60;
+  if (hours === 0) {
+    return `${minutes}м`;
+  } else if (minutes === 0) {
+    return `${hours}ч`;
+  } else {
+    return `${hours}ч ${minutes}м`;
+  }
+};
+
+export function filterShorts(movies) {
+  return movies.filter((movie) => movie.duration < 40);
+}
