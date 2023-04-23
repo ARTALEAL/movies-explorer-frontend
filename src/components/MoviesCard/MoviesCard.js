bc447546fcb5ad769284008bffb1a73065cfb1c9
ARTALEAL <artaleal@yandex.ru>
2023-04-23 20:52:22 +0300

    card function test

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import './MoviesCard.css';
+import { convertMinToHours } from '../../utils/utils';
 
 const MoviesCard = ({ isSavedMoviesPage, movie, onSave, onDelete }) => {
+  const handleSaveCard = () => {
+    onSave(movie);
+  };
+
+  const handleDeleteCard = () => {
+    onDelete(movie);
+  };
+
   return (
     <div className="card">
       <img
@@ -15,15 +24,19 @@ const MoviesCard = ({ isSavedMoviesPage, movie, onSave, onDelete }) => {
       />
       <div className="card__description">
         <span className="card__name">{movie.nameRU}</span>
-        <span className="card__duration">{movie.duration}</span>
+        <span className="card__duration">
+          {convertMinToHours(movie.duration)}
+        </span>
       </div>
       {movie.saved && !isSavedMoviesPage && (
-        <button className="card__button_saved" />
+        <button className="card__button_saved" onClick={handleSaveCard} />
       )}
       {isSavedMoviesPage ? (
-        <button className="card__button_delete" />
+        <button className="card__button_delete" onClick={handleDeleteCard} />
       ) : (
-        <button className="card__button">Сохранить</button>
+        <button className="card__button" onClick={handleSaveCard}>
+          Сохранить
+        </button>
       )}
     </div>
   );
